ad1eef4d3f8a62f4e97fa676f8579a2e80ce63d1
kidusfmariam <99491498+kidusfmariam@users.noreply.github.com>
2023-01-02 01:14:57 -0800

    Styling Bug Fix

--- src/Containers/Newsletter/Newsletter.jsx
+++ src/Containers/Newsletter/Newsletter.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './Newsletter.css'
 const Newsletter = () => {
   return (
     <section className='newsletter-wrapper'>
-        <h1>SUBSCRIBE TO OUR NEWSLETTER</h1>
-        <input type="email" placeholder='Email' />
-        <button type='submit'>Subscribe</button>
+      <img src="/Assests/Contact Page/Img-2.jpg" alt="burger" className='bg-img' />
+        <motion.h1
+        initial={{opacity: 0}}
+        whileInView={{opacity: 1}}
+        transition={{duration: 1.2, delay: 0.3}}
+        viewport={{once: true}}
+        >SUBSCRIBE TO OUR NEWSLETTER</motion.h1>
+        <motion.input 
+        initial={{opacity: 0, y: 100}}
+        whileInView={{opacity: 1, y: 0}}
+        viewport={{once: true}}
+        transition={{duration: 1.4, delay: 0.4}}
+        type="email" placeholder='Email' />
+        <motion.button 
+        initial={{opacity: 0, y: 200}}
+        whileInView={{opacity: 1, y: 0}}
+        viewport={{once: true}}
+        transition={{duration: 1.4, delay: 0.8}}
+        type='submit'>Subscribe</motion.button>
     </section>
   )
 }
